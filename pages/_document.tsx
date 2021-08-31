4c6f35e705fe5f40f4c1ebf0c376899bedefcd7c
dheeraj-grover <itleaders@gmail.com>
2021-08-31 15:45:34 +0530

    changed _document.tsx with new pixel code.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, {
   Head,
   Html,
   Main,
-  NextScript
+  NextScript,
 } from 'next/document';
 import { Children } from 'react';
 import { ServerStyleSheet } from 'styled-components';
@@ -16,36 +16,29 @@ export default class BivocalBirdsDoc extends Document {
           <link rel='shortcut icon' href='/favicon.ico' />
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`,
+              __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '600194914309916');
+      fbq('track', 'PageView');
+      `,
             }}
           />
+          <noscript>
+            <img
+              height='1'
+              width='1'
+              style={{ display: 'none' }}
+              src='https://www.facebook.com/tr?id=600194914309916&ev=PageView&noscript=1'
+            />
+          </noscript>
           <script async src='https://r.wdfl.co/rw.js' data-rewardful='a0717b' />
-
-<!-- Facebook Pixel Code -->
-<script>
-  !function(f,b,e,v,n,t,s){
-  if(f.fbq) return;
-  n=f.fbq=function(){
-    n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
-  };
-  if(!f._fbq) f._fbq=n;
-  n.push=n;
-  n.loaded=!0;
-  n.version='2.0';
-  n.queue=[];
-  t=b.createElement(e);
-  t.async=!0;
-  t.src=v;
-  s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}
-  (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '600194914309916');
-  fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=600194914309916&ev=PageView&noscript=1" /></noscript>
-<!-- End Facebook Pixel Code -->
-
-
         </Head>
         <body>
           <Main />
