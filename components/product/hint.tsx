62e42753e734c7384f16bec28f27014ff4aec624
itleaders <itleaders@gmail.com>
2024-04-04 21:09:02 +0530

    test

--- components/product/hint.tsx
+++ components/product/hint.tsx
@@ -3,7 +3,7 @@ import { StyledHint } from './style';
 export default function Hint() {
   return (
     <StyledHint>
-      <small>Cancel Anytime</small>
+      <small>Cancel Anytime</small><br /><small>Cancel Anytime</small>
     </StyledHint>
   );
 }
